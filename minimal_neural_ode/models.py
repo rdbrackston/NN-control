9fddfdacac58926441ebe4c1cf4f199c758ee035
Dave <schnoerr@gmail.com>
2019-10-09 11:49:17 +0100

    Explicitly initialise weights of linear models

--- minimal_neural_ode/models.py
+++ minimal_neural_ode/models.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 keras = tf.keras
+from keras import initializers
 
 # oscillator as ODE within Keras
 class oscillator(tf.keras.Model):
@@ -72,7 +73,8 @@ class oscillator_linear_control(tf.keras.Model):
     
     def __init__(self):
         super(oscillator_linear_control, self).__init__()
-        self.linear = keras.layers.Dense(1, input_shape=(2,))
+        self.linear = keras.layers.Dense(1, input_shape=(2,), kernel_initializer=initializers.random_normal(stddev=0.01))
+#         self.linear = keras.layers.Dense(1, input_shape=(2,), kernel_initializer=keras.initializers.Constant(value=[-23,-10]))
         self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
         
     def forward(self, t, y):
@@ -90,7 +92,8 @@ class oscillator_linear_control_sde(tf.keras.Model):
     
     def __init__(self):
         super(oscillator_linear_control_sde, self).__init__()
-        self.linear = keras.layers.Dense(1, input_shape=(2,))
+#         self.linear = keras.layers.Dense(1, input_shape=(2,))
+        self.linear = keras.layers.Dense(1, input_shape=(2,), kernel_initializer=initializers.random_normal(stddev=0.01))
         self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
         
     def forward(self, t, y):
