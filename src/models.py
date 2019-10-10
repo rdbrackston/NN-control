246e420e4893461af93c5196587e4d9902f36037
Rowan Brackston <rowan.brackston@cantab.net>
2019-10-10 14:55:53 +0100

    Van der Pol oscillator

--- src/models.py
+++ src/models.py
@@ -2,6 +2,38 @@ import tensorflow as tf
 keras = tf.keras
 from keras import initializers
 
+# Van der Pol oscillator
+class VDPOscillator(tf.keras.Model):
+    def __init__(self):
+        self.mu = tf.cast(1.0, tf.float32)
+
+    def forward(self, t, y):
+        x = y[0,0,0]
+        dx = y[0,1,0]
+        dy = [[dx], [self.mu*(1-tf.math.square(x))*dx-x]]
+        return tf.stack(dy)
+
+
+# Van der Pol oscillator with control
+class VDPOscillatorNNControl(tf.keras.Model):
+
+    def __init__(self):
+        super(VDPOscillatorNNControl, self).__init__()
+        self.mu = tf.cast(1.0, tf.float32)
+        self.K = keras.Sequential()
+        self.K.add(keras.layers.Dense(50, activation="tanh", input_shape=(2,)))
+        self.K.add(keras.layers.Dense(1))
+
+    def forward(self, t, y):
+        x = y[0,0,0]
+        dx = y[0,1,0]
+        free = [[dx], [self.mu*(1-tf.math.square(x))*dx-x]]
+        con = self.K(tf.compat.v2.squeeze(y,2))
+        con = tf.concat([tf.zeros(con.shape),con],1)
+        con = tf.reshape(con, [-1,2,1]) # Expand to match dimensions
+        return tf.add(tf.stack(free), con)
+
+
 # oscillator as ODE within Keras
 class Oscillator(tf.keras.Model):
     def __init__(self):
@@ -9,7 +41,6 @@ class Oscillator(tf.keras.Model):
 
     def forward(self, t, y):
         return tf.matmul(self.A, y)
-
 
 
 class OscillatorNNControl(tf.keras.Model):
@@ -46,7 +77,7 @@ class OscillatorLinearControl(tf.keras.Model):
         self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
         
     def forward(self, t, y):
-        y = tf.reshape(y,[-1,2,1]) # Ensure y has correct dimensions
+        # y = tf.reshape(y,[-1,2,1]) # Ensure y has correct dimensions
         con = self.linear(tf.compat.v2.squeeze(y,2))
         free = tf.matmul(self.A, y)
         con = tf.concat([tf.zeros(con.shape),con],1)
