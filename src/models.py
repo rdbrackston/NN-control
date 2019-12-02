bf83c5bdcd5adf0be6cd5ae5abafe5cba8746ff7
David Schnoerr <schnoerr@gmail.com>
2019-12-02 11:30:17 +0000

    Reverse to old version
    Had accidentically changed this file instead of file in src_v2 to 
    migrate to tf 2.0

--- src/models.py
+++ src/models.py
@@ -48,14 +48,14 @@ class OscillatorNNControl(tf.keras.Model):
     Oscillator with controller defined by dense sequential neural network.
     Suitable for use with batch inputs.
     """
-    
+
     def __init__(self):
         super(OscillatorNNControl, self).__init__()
         self.K = keras.Sequential()
         self.K.add(keras.layers.Dense(50, activation="tanh", input_shape=(2,)))
         self.K.add(keras.layers.Dense(1))
         self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
-        
+
     def forward(self, t, y):
         y = tf.reshape(y,[-1,2,1]) # Ensure y has correct dimensions
         con = self.K(tf.compat.v2.squeeze(y,2))
@@ -69,13 +69,13 @@ class OscillatorNNControl(tf.keras.Model):
 
 # Oscillator with controller defined by neural network
 class OscillatorLinearControl(tf.keras.Model):
-    
+
     def __init__(self):
         super(OscillatorLinearControl, self).__init__()
         self.linear = keras.layers.Dense(1, input_shape=(2,), kernel_initializer=initializers.random_normal(stddev=0.01))
 #         self.linear = keras.layers.Dense(1, input_shape=(2,), kernel_initializer=keras.initializers.Constant(value=[-23,-10]))
         self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
-        
+
     def forward(self, t, y):
         # y = tf.reshape(y,[-1,2,1]) # Ensure y has correct dimensions
         con = self.linear(tf.compat.v2.squeeze(y,2))
@@ -84,4 +84,3 @@ class OscillatorLinearControl(tf.keras.Model):
         con = tf.reshape(con, [-1,2,1]) # Expand to match dimensions
         dy = tf.add(free, con)
         return dy
-
