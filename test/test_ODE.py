fd761ef3db0f15bb036ee59e4d56bf8af70fa9a1
Rowan Brackston <rowan.brackston@cantab.net>
2020-01-17 17:43:14 +0000

    Update test scripts

--- test/test_ODE.py
+++ test/test_ODE.py
@@ -1,80 +1,56 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import tensorflow.contrib.eager as tfe
-
-keras = tf.keras
-tf.compat.v1.enable_eager_execution()
 
 import sys
-sys.path.insert(0, "..")
-from neuralode_mod import NeuralODE
-from neuralsde import compute_gradients_and_update_path
+sys.path.insert(0, "src")
 
-# oscillator as ODE
-class ode_oscillator(tf.keras.Model):
+import importlib
+from euler import fwd, fwd_tlist, fwd_sde_tlist
+importlib.reload(sys.modules['euler'])
+import models as md
+importlib.reload(sys.modules['models'])
+import training as tr
+importlib.reload(sys.modules['training'])
+from utilities import rand_batch, tile_batch
+importlib.reload(sys.modules['utilities'])
 
-    def __init__(self):
-        super(ode_oscillator, self).__init__()
-        self.A = tf.cast([[0, 1],[-1, -0.1]], tf.float32)
-
-    def call(self, inputs, **kwargs):
-        t, y = inputs
-        return tf.matmul(y, self.A)
-
-time_steps = 1000
-t_grid = np.linspace(0, 100, time_steps)
-y0 = tf.cast([[5., 5.]], tf.float32)
-
-model = NeuralODE(ode_oscillator(), t=t_grid)
-yN, free_trajectory = model.forward(y0, return_states="numpy")
-free_trajectory = np.concatenate(free_trajectory)
-
-plt.plot(t_grid,free_trajectory[:,0])
-plt.savefig("FreeODE.pdf", format='pdf')
-
-# Oscillator with controller defined by neural network
-class ode_oscillator_NNcontrol(tf.keras.Model):
-    
-    def __init__(self):
-        super(ode_oscillator_NNcontrol, self).__init__()
-        self.linear1 = keras.layers.Dense(50, activation="tanh")
-        self.linear2 = keras.layers.Dense(1)
-        self.A = tf.cast([[0, 1],[-1, -0.1]],tf.float32)
-        
-    def call(self, inputs, **kwargs):
-        t, y = inputs
-        con = self.linear1(y)
-        con = self.linear2(con)
-        dy = tf.add(tf.matmul(y, self.A), tf.concat([tf.cast([[0.0]],tf.float32),con],1))
-        return dy
+t_end = 50
+t_grid = np.linspace(0, t_end, t_end/0.01)
+y0 = tf.constant([[5., 5.]], tf.float32)
+free_trajectory = fwd_tlist(md.Oscillator(), y0, t_grid,0.01)
+plt.clf()
+plt.plot(t_grid,free_trajectory[0:len(t_grid),0,0])
 
 # Initialise new instances of the model
-model = ode_oscillator_NNcontrol()
-neural_ode = NeuralODE(model, t=t_grid)
-neural_ode_test = NeuralODE(model, t=t_grid)
-
+model = md.OscillatorNNControl()
 niters = 5
 loss_history = []
 optimizer = tf.compat.v1.train.MomentumOptimizer(learning_rate=1e-5, momentum=0.95)
 
-# Run simulation niters times, updating parameters on each iteration
-for step in range(niters+1):
-    
-    print("Running iteration %i" %(step))
-    batch_y0 = y0    # Fixed initial condition
-    # batch_y0 = tf.random_uniform([1,2], minval=-15.,maxval=15.) # Random
-    
-    loss = compute_gradients_and_update_path(batch_y0, neural_ode, optimizer, True)
-    loss_history.append(loss.numpy())
-    
-    if step % 1 == 0:
-        print(loss)
+def train_ode(model, batch_size, niters, test_freq, t_grid, free_trajectory):
+    y0 = tf.constant([[[1.], [0.]]],tf.float32)
+    optimizer = tf.keras.optimizers.Adamax(learning_rate=1e-2)
+    loss_history = []
 
+    for step in range(niters+1):
+        batch_y0 = tile_batch(batch_size, y0) # Fixed
+        loss = tr.gradient_update(model, fwd_tlist, optimizer, batch_y0, t_grid)
+        loss_history.append(loss.numpy())
 
-yN, trajectory = neural_ode_test.forward(y0, return_states="numpy")
-trajectory = np.concatenate(trajectory)
+        if step % test_freq == 0:
+            print(loss)
+
+    return loss_history
+
+batch_size = 5
+niters = 5
+test_freq = 5
+
+loss_history = train_ode(model, batch_size, niters, test_freq, t_grid, free_trajectory)
+trajectory = fwd_tlist(model, y0, t_grid,0.01)
 plt.clf()
-plt.plot(t_grid, free_trajectory[:,0], t_grid, trajectory[:,0])
-plt.legend( ('free oscillator', 'controlled oscillator/system'))
+plt.plot(t_grid,free_trajectory[0:len(t_grid),0,0],\
+        t_grid,trajectory[0:len(t_grid),0,0,0])
+plt.legend( ('free oscillator', 'controlled oscillator'))
 plt.savefig("ControlledODE.pdf", format='pdf')
