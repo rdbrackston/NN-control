f8b65ceb2489b7a87732ff748f24450bdaca4dec
Rowan Brackston <rowan.brackston@cantab.net>
2020-01-22 18:57:13 +0000

    Correct naming clash

--- src/euler.py
+++ src/euler.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 from copy import deepcopy
 
-def fwd(mdl,y0,tf):
+def fwd(mdl,y0,tEnd):
     t = 0
     h =0.01
     y = deepcopy(y0)
     traj =[]
-    while t <= tf:
+    while t <= tEnd:
         traj.append(deepcopy(y))
 #         print(y)
         dy = mdl.forward(0, y)
@@ -26,13 +26,13 @@ def fwd_tlist(mdl,y0,tlist,step):
         y = tf.reshape(y0, [-1,n,1])
     else:
         y = deepcopy(y0)
-    
+
     t_ind = 0
     while t < tlist[-1]:
         dy = mdl.forward(t, y)
         t += h
         y = y + h * dy
-        
+
         if t >= tlist[t_ind]:
             traj.append(1*y)
             t_ind += 1
@@ -57,7 +57,7 @@ def fwd_sde_tlist(mdl,y0,tlist,step):
     g = tf.zeros([1,n-1,1])
     g = tf.concat([g, tf.cast([[[0.5]]],tf.float32)],1)
     g = tf.tile(g,[tf.shape(y)[0],1,1])
-    
+
     t_ind = 0
     while t < tlist[-1]:
         # f,g = mdl.forward(t, y)
@@ -65,9 +65,8 @@ def fwd_sde_tlist(mdl,y0,tlist,step):
         dW = tf.random.normal(tf.shape(g))*rtdt
         t += dt
         y += f*dt + g*dW
-        
+
         if t >= tlist[t_ind]:
             traj.append(1*y)
             t_ind += 1
     return tf.stack(traj)
-
